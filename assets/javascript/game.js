853e7a79f9b9f1c093dd8996159f150f0f452d71
Jon Berry <jonpber@gmail.com>
2017-05-02 07:57:56 -0700

    word generation working in early stages.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,16 +8,37 @@ letGuessed = "ABC";
 
 playing = false;
 
-var words = ["cat"]
+var words = ["cat", "sword", "trickle"]
+
+var letters = document.getElementsByClassName("letterInWord");
+var spaces = document.getElementsByClassName("letterSpaces");
 
 function resetGame () {
 	document.getElementById("numberOfWins").innerHTML = wins;
 	document.getElementById("guessesRem").innerHTML = remGuesses;
 	document.getElementById("lettersGuessed").innerHTML = letGuessed;
+
+    var i;
+    for (i = 0; i < spaces.length; i++) {
+        spaces[i].style.display = "none";
+        letters[i].style.display = "none";
+    }
 }
 
-function gameOn () {
-	document.getElementById("gameStateText").innerHTML = "Guess the word!";
+function gameClick () {
+	if (playing == false){
+		document.getElementById("gameStateText").innerHTML = "Guess the word!";
+		playing = true;
+
+		for (var i = 0; i < words[0].length; i++) {
+			letters[i].innerHTML = words[0][i];
+			spaces[i].style.display = "block";
+		}
+	}
+
+	else {
+
+	}
 }
 
-document.onkeypress = gameOn;
+document.onkeypress = gameClick;
