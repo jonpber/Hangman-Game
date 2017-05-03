6442608140ace2a027f8a30150f1239cf2fc4d09
Jon Berry <jonpber@gmail.com>
2017-05-03 09:57:04 -0700

    fixed layout to be consistent; added win and lose sounds.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,6 +16,9 @@ var spaces = document.getElementsByClassName("letterSpaces");
 
 var successSound = new Audio("assets/sounds/success.wav")
 var failSound = new Audio("assets/sounds/fail.wav")
+var winSound = new Audio("assets/sounds/win.wav")
+var loseSound = new Audio("assets/sounds/lose.wav")
+
 // 
 
 function resetGame () {
@@ -65,7 +68,7 @@ function gameClick () {
 					}
 
    					for (var i = 0; i < wordToGuess.length; i++) {
-   						if (wordToGuess[i] == keynum.toUpperCase()){
+   						if (wordToGuess[i] === keynum.toUpperCase()){
    							letters[i].innerHTML = keynum.toUpperCase();
    							letters[i].style.display = "block";
    							gameStateCheck();
@@ -102,6 +105,7 @@ function changeDocValue(idName, value){
 function gameStateCheck(){
 	if (remGuesses < 1){
 		changeDocValue("gameStateText", "Game Over! Click to start again.");
+		loseSound.play();
 		playing = false;
 	}
 	else {
@@ -110,11 +114,12 @@ function gameStateCheck(){
 			placeHolderWord += letters[i].textContent;
 		}
 
-		if (wordToGuess == placeHolderWord){
+		if (wordToGuess === placeHolderWord){
 			wins += 1;
 			changeDocValue("numberOfWins", wins);
 			playing = false;
 			changeDocValue("gameStateText", "You Win! Click to start again.");
+			winSound.play();
 
 		}
 	}
